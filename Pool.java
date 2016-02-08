553acadcf121d9225ad03e94eba6b915f7f014dc
Jason <jaysonia@gmail.com>
2016-02-08 20:29:54 +0000

    Update Pool.java

--- Pool.java
+++ Pool.java
@@ -1,151 +1,68 @@
 package scrabble;
 
-import java.util.EmptyStackException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.Vector;
-
 
 public class Pool {
-	//initialize variables
-	private Vector<Character> tiles = new Vector<Character>();
-	private int ascii = (int) 'a';
-	private static final HashMap<Character, Integer> tileValue;
-	static{
-		tileValue = new HashMap<Character, Integer>();
-		tileValue.put('#', 0);
-		tileValue.put('a', 1);
-		tileValue.put('b', 3);
-		tileValue.put('c', 3);
-		tileValue.put('d', 2);
-		tileValue.put('e', 1);
-		tileValue.put('f', 4);
-		tileValue.put('g', 2);
-		tileValue.put('h', 4);
-		tileValue.put('i', 1);
-		tileValue.put('j', 8);
-		tileValue.put('k', 5);
-		tileValue.put('l', 4);
-		tileValue.put('m', 3);
-		tileValue.put('n', 1);
-		tileValue.put('o', 1);
-		tileValue.put('p', 3);
-		tileValue.put('q', 10);
-		tileValue.put('r', 1);
-		tileValue.put('s', 1);
-		tileValue.put('t', 1);
-		tileValue.put('u', 1);
-		tileValue.put('v', 4);
-		tileValue.put('w', 4);
-		tileValue.put('x', 8);
-		tileValue.put('y', 4);
-		tileValue.put('z', 10);
-	}
+
+	private static final int NUM_BLANKS = 2;
+	private static final int[] NUM_TILES = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};  // A-Z
 	
-	private static final HashMap<Character, Integer> tileQuant;
-	static{
-		tileQuant = new HashMap<Character, Integer>();
-		tileQuant.put('#', 2); 
-		tileQuant.put('a', 9); 
-		tileQuant.put('b', 2); 
-		tileQuant.put('c', 2); 
-		tileQuant.put('d', 4); 
-		tileQuant.put('e', 12);
-		tileQuant.put('f', 2);
-		tileQuant.put('g', 3);
-		tileQuant.put('h', 2);
-		tileQuant.put('i', 9);
-		tileQuant.put('j', 1);
-		tileQuant.put('k', 1);
-		tileQuant.put('l', 4);
-		tileQuant.put('m', 2);
-		tileQuant.put('n', 6);
-		tileQuant.put('o', 8);
-		tileQuant.put('p', 2);
-		tileQuant.put('q', 1);
-		tileQuant.put('r', 6);
-		tileQuant.put('s', 4);
-		tileQuant.put('t', 6);
-		tileQuant.put('u', 4);
-		tileQuant.put('v', 2);
-		tileQuant.put('w', 2);
-		tileQuant.put('x', 1);
-		tileQuant.put('y', 2);
-		tileQuant.put('z', 1);
-	}
+	private Random randomGenerator = new Random();
+
+	private ArrayList<Tile> pool = new ArrayList<Tile>();
 	
-	//Constructor
-	public Pool(){
-		//add the blank tile value
-		for(int i=0;i< tileQuant.get('#');i++){
-			tiles.add('#');
+	Pool () {
+		Tile tile;
+		char face;
+		for (int i=0; i<NUM_BLANKS; i++) {
+			tile = new Tile(Tile.BLANK);
+			pool.add(tile);
 		}
-		
-		//add all the tiles
-		for(int i = ascii;i<ascii+26;i++){
-			for(int j = 0;j<tileQuant.get((char) i);j++){
-				tiles.add((char) i);
+		face = 'A';
+		for (int i=0; i<NUM_TILES.length; i++) {
+			for (int j=0; j<NUM_TILES[i]; j++) {
+				tile = new Tile(face);
+				pool.add(tile);
 			}
-		}
-				
-	}
-	
-	public void reset(){
-		//reset the size of the pool to 0 and reset the Vector
-		tiles.clear();
-		
-		//add the blank tile value
-		for(int i=0;i< tileQuant.get('#');i++){
-			tiles.add('#');
-		}
-		
-		//add all the tiles
-		for(int i = ascii;i<ascii+26;i++){
-			for(int j = 0;j<tileQuant.get((char) i);j++){
-				tiles.add((char) i);
-			}
-		}
-	}
-	
-	//Check if the pool is empty
-	public boolean isEmpty(){
-		if(tiles.isEmpty())
-			return true;
-		else
-			return false;
-	}
-	
-	//returns the size of the pool
-	public int poolSize(){
-		return tiles.size();
-	}
-	
-	//draw a random tile
-	public char tileDraw(){
-		Random random = new Random();
-		char temp;
-		
-		//test if stack is empty before drawing a tile
-		if(tiles.isEmpty()){
-			throw new EmptyStackException();
-		}
-		
-		else{
-			int randInt = random.nextInt(tiles.size());
-			temp = tiles.get(randInt);
-			tiles.remove(randInt);
-			return temp;
-		}
-	}
-	
-	//return the tile value for a given tile
-	public int tileVal(char tile){		
-		return tileValue.get(tile);
-	}
-	
-	//returns a tile to the pool
-	public void returnTile(char tile){
-		tiles.add(tile);
+			face++;
+		}	
+		return;
 	}
 
+	public int size () {
+		return(pool.size());
+	}
+	
+	public boolean isEmpty () {	
+		return(pool.isEmpty());
+	}		
+	
+	public void add (Tile tile) {
+		pool.add(tile);
+	}
+			
+	public Tile getRandomTile () {
+		int index = randomGenerator.nextInt(pool.size());
+		return(pool.get(index)); 
+	}
+	
+	public ArrayList<Tile> draw (int numRequested) {
+		int numGiven;
+		ArrayList<Tile> drawnTiles = new ArrayList<Tile>();
+		Tile tile;
+		
+		if (numRequested > pool.size())
+			numGiven = pool.size();
+		else {
+			numGiven = numRequested;
+		}
+		for (int i=0; i<numGiven; i++) {
+			tile = getRandomTile();
+			drawnTiles.add(tile);
+			pool.remove(tile);
+		}	
+		return(drawnTiles);
+	}
+	
 }
